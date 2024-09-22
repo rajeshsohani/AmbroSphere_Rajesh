91ddee693e2d01752070a4c67faebbfc88ee690e
rajeshsohani <78219018+rajeshsohani@users.noreply.github.com>
2024-09-22 11:08:22 +0530

    Update settings.py

--- WebSiteBase/settings.py
+++ WebSiteBase/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4$#+ljvlr7t*9$@n_te!-dn&e(mng^!jd_fq5=(1rlhlovsose'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ambrosphere_rajesh.onrender.com']
 
 
 # Application definition
